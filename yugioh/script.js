15624decb2a023a02847670d5c18fafe6d694936
Shihan Ai <g3aishih@gmail.com>
2021-08-26 23:26:53 -0400

    add yugioh

---
+++ yugioh/script.js
@@ -0,0 +1,21 @@
+function loadJSON(callback) {
+    var xObj = new XMLHttpRequest();
+    xObj.overrideMimeType("application/json");
+    xObj.open('GET', './yugioh.json', true);
+    xObj.onreadystatechange = function() {
+        if (xObj.readyState === 4 && xObj.status === 200) {
+            // 2. call your callback function
+            callback(xObj.responseText);
+        }
+    };
+    xObj.send(null);
+}
+  
+function init() {
+    loadJSON(function(response) {
+        var json = JSON.parse(response);
+        console.log(json);
+    });
+}
+
+init();
